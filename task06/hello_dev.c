a5050773de492ae732581f848047197a3b60c8cc
Jason Turley <jasonturley0@gmail.com>
2021-08-12 20:08:35 -0500

    refactored and added comments

--- task06/hello_dev.c
+++ task06/hello_dev.c
@@ -1,3 +1,18 @@
+/**
+ * 
+ * To install and create /dev/eudyptula: 
+ *	make && make insert
+ *
+ * Test read function:
+ * 	cat /dev/eudyptula
+ *	abc123
+ *
+ * Test write function:
+ * 	echo "abc123" > /dev/eudyptula && echo $?
+ * 
+ * To remove:
+ *	make remove
+ */
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -8,18 +23,19 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jason Turley");
+MODULE_DESCRIPTION("Misc char device hello world module");
 
-#define HELLO_ID "abc123"
+#define HELLO_ID "abc123"	/* Fictional ID number */
+#define HELLO_ID_LEN 7		/* Length of ID plus NULL byte */
 
-static struct miscdevice hello_misc;
 
 /**
  * Returns my assigned id when the character device is read from. 
  */
-static ssize_t eudyptula_read(struct file *fp, char __user *buf, size_t count, 
+static ssize_t eudyptula_read(struct file *fp, char __user *user_buf, size_t count, 
 	loff_t *pos)
 {
-	return simple_read_from_buffer(buf, count, pos, 
+	return simple_read_from_buffer(user_buf, count, pos, 
 		HELLO_ID, strlen(HELLO_ID));
 }
 
@@ -28,51 +44,47 @@ static ssize_t eudyptula_read(struct file *fp, char __user *buf, size_t count,
  * then this function returns a correct write return value. Otherwise, returns
  * the "invalid value" error value.
  */
-static ssize_t eudyptula_write(struct file *fp, const char __user *buf, 
+static ssize_t eudyptula_write(struct file *fp, const char __user *user_buf, 
 	size_t count, loff_t *pos)
 {
-	ssize_t nread;
-	char *msg = kmalloc(count + 1, GFP_KERNEL);
+	ssize_t ret;
+	char buf[HELLO_ID_LEN];
 
-	if (!msg)
-		return -ENOMEM;
+	memset(buf, 0, HELLO_ID_LEN);
 
-	nread = simple_write_to_buffer(msg, count, pos, buf, count);
+	/* Don't trust user data: safely copy it to `buf` */
+	ret = simple_write_to_buffer(buf, HELLO_ID_LEN, pos, user_buf, count);
 
-	if (strncmp(msg, HELLO_ID, strlen(HELLO_ID)) != 0)
+	/* Compare user written value to my ID */
+	if (strncmp(buf, HELLO_ID, HELLO_ID_LEN -1))
 		return -EINVAL;
-
-	kfree(msg);
-	return nread;
+		
+	return ret;
 }
 
 
 static const struct file_operations hello_fops = {
 	.owner = THIS_MODULE,
 	.read  = eudyptula_read,
-	.write = eudyptula_write,	
+	.write = eudyptula_write
 };
 
 
+static struct miscdevice hello_misc = {
+	MISC_DYNAMIC_MINOR,
+	"eudyptula",
+	&hello_fops
+};
+
 static int __init eudyptula_init(void)
 {
-	pr_debug("eudyptula: module loaded!\n");
-
-	/* Let the kernel pick our minor number for us */
-	hello_misc.minor = MISC_DYNAMIC_MINOR;
-
-	/* Name our misc device /dev/eudyptula */
-	hello_misc.name = "eudyptula";
-
-	/* What functions to call when a program uses file operations */
-	hello_misc.fops = &hello_fops;
-	
+	printk(KERN_DEBUG "hello_dev module loaded!\n");
 	return misc_register(&hello_misc);
 }
 
 static void __exit eudyptula_exit(void)
 {
-	pr_debug("eudyptula: module unloaded!\n");
+	printk(KERN_DEBUG "hello_dev module unloaded!\n");
 	misc_deregister(&hello_misc);
 }
 
